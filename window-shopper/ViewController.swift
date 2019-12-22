037069dab3e62b63de292ecfdb1ea2eaa33d69f9
Matthias Plancke <matthiasplancke@MacBook-Air-van-Matthias.local>
2019-12-22 15:49:18 +0000

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Matthias Plancke on 22/12/2019.
+//  Copyright © 2019 Matthias Plancke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
